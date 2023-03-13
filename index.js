040f1bae409ea3dca6874463ba7ebfd808bd5cb9
tati manel <108736581+Toutouhelwa@users.noreply.github.com>
2023-03-13 21:12:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ let countEl = document.getElementById("count-el")
 let string = document.getElementById("string-el")
  var count = 0;
 function increment() {
-    count+=1;
-    countEl.innerText=count;
+    count+=1
+    countEl.innerText=count
 }
 function save (){
     let counster = count+" - "
     string.textContent+=counster
+    countEl.innerText = 0
+    count= 0
 }
-save()
+
+
